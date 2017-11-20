a9187e08787bf1fc3043437b476ad0b4789f6952
Josneyo <jdavidcastan@hotmail.com>
2017-11-19 23:47:52 -0200

    New approach to the application
    Mudei completamente a forma como o sistema será realizado, modifiquei todas as dependencias do node e estou testando utilizar o pug, para melhor escalabilidade da página.

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
 
 var app = express();
+// app.use(bodyParser.urlencoded({ extended: false}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// // app.use(express.static(__dirname + '/public'));
 
-app.use('/', index);
-app.use('/users', users);
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// app.use(express.static(__dirname + '/public'));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+app.use("/",router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +45,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(8080, () => {
+  console.log('The application is running on localhost:8080!');
+});
